86a318dabcb620668f414aad0b249b8268409a4a
Masahiro Nagata <g2114023@fun.ac.jp>
2015-08-06 15:45:54 +0900

    距離計を追加した

---
+++ unit/DistanceMeter.cpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "DistanceMeter.h"
+
+DistanceMeter::DistanceMeter(Motor &rm, Motor &lm){
+	Rmotor = &rm;
+	Lmotor = &lm;
+	rDistance = 0;
+	lDistance = 0;
+	init();
+}
+
+void DistanceMeter::init(){
+	lOffset = Lmotor->getCount();
+	rOffset = Rmotor->getCount();
+}
+
+double DistanceMeter::getDistance(){
+	const double DPOD = 0.06981317F;
+	double distance = 0.0;
+
+	rDistance = Rmotor->getCount() - rOffset;
+	lDistance = Lmotor->getCount() - lOffset;
+
+	distance = (rDistance + lDistance)/2;
+	distance = distance * DPOD;
+
+	return distance;
+}
--- unit/DistanceMeter.h
+++ unit/DistanceMeter.h
@@ -2,14 +2,19 @@
 
 #include "Motor.h"
 
+using namespace ev3api;
+
 class DistanceMeter{
 public:
-	DistanceMeter();
-	double calcDistance();
+	DistanceMeter(Motor &rm, Motor &lm);
+	void init();
+	double getDistance();
 
 private:
 	Motor *Rmotor;
 	Motor *Lmotor;
 	int32_t lOffset, rOffset;
-	
-}
+	double rDistance, lDistance;
+
+
+};
