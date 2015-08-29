4925aaeff962cbac9cbf08d623c7d3a22b605c8d
Masahiro Nagata <g2114023@fun.ac.jp>
2015-08-30 02:51:20 +0900

    add stepper

--- SFigureL.cpp
+++ SFigureL.cpp
@@ -1,9 +1,18 @@
 
 #include "SFigureL.h"
 
-SFigureL::SFigureL(){
+SFigureL::SFigureL(DistanceMeter *dm, Drive *dr, LineTracer *lt, Observer *ob){
+	phase = 1;
 }
 
 bool SFigureL::run(){
+	// switch(phase) {
+	// 	case 1: // 段差を登るlog
+	// 	case 2: // 右エッジでライントレースする
+	// 	case 3: // 黒色を検知する
+	// 	case 4: // 右に90度旋回する
+	// 	case 5: // 右エッジでライントレースする
+	// 	case 6: // 段差を登る
+	// }
 	return false;
 }
