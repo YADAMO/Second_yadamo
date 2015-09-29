f1ffa876df2ab198b4dcc76e2ca1777aed519666
KentoYamamoto <g2114034@fun.ac.jp>
2015-09-29 21:18:27 +0900

    impl gijigengo

--- LineReturn.cpp
+++ LineReturn.cpp
@@ -12,16 +12,24 @@ bool LineReturn::run(){
 	bool end = false;
 	switch(phase){
 		case 0:
-			
+			//色検出もーどで色検知
+			//左に旋回する
+				//もしラインがあったならケース1へ
+				//ラインがなかったならケース2へ
 			break;
 		case 1:
-			
+			//ある程度右へ旋回する　その後ケース4へ
 			break;
 		case 2:
-			
+			//色検出モードで色検知
+			//右旋回する
+				//ラインがあったならケース3へ
 			break;
 		case 3:
-			
+			//ある程度←へ旋回する　その後ケース4へ
+			break;
+		case 4:
+			//ライントレースする
 			break;
 		default:
 			break;
