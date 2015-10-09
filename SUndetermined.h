18e0d3e288fcb9d4e4fc27ffea0b0675dd7f0ee8
KentoYamamoto <g2114034@fun.ac.jp>
2015-10-09 22:25:00 +0900

    merge kikuyacode

--- SUndetermined.h
+++ SUndetermined.h
@@ -1,11 +1,38 @@
 #pragma once
+#include <stdlib.h>
+//#include <vector>
 
+#include "SBarcode.h"
+#include "Observer.h"
+#include "Drive.h"
+#include "DistanceMeter.h"
 #include "ScenarioController.h"
+#include "LineReturn.h"
+#include "LineTrace.h"
 
 class SUndetermined : public ScenarioController{
 public:
-	SUndetermined();
+
+    SBarcode *barCode;
+    Drive *drive;
+    Observer *observer;
+    LineReturn *lineReturn;
+    LineTrace *lineTrace;
+
+	SUndetermined(SBarcode *bc, Drive *dri, Observer *obs,LineReturn *lt);
+   // int getRunPattern(std::vector<int> bit_array);
     virtual bool run();
 
 private:
+    int phase;
+    int run_on_board_pattern;
+    int start_position;
+    
+    int calcManhattanDistance(int pos_1, int pos_2);
+    int getRunPattern(unsigned char c);
+    int checkRoutePattern(int p1, int p2);
+    //Point2 decideStartPosition(std::vector<Point2> p_vec);
+    // std::vector<Point2> bitary2BottlesPos(std::vector<int> bit_array);
+    // std::vector<int> uchar2bitary(unsigned char c);
+
 };
